eac666e150cd96c34f632bff676b57df62fe4b52
马琳峰 <80527573@qq.com>
2021-03-29 17:01:37 +0800

    替换接口生成

--- index.js
+++ index.js
@@ -11,59 +11,63 @@ const destDir = process.cwd()
 const path = require('path')
 // 文件操作
 const fs = require('fs')
+// http
+const https = require('https')
 
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'Project name ?',
-  },
-  {
-    type: 'input',
-    name: 'version',
-    message: 'What version?',
-    default: '0.0.1'
-  },
-  {
-    type: 'list',
-    message: 'Please select a project template',
-    name: 'template',
-    choices: [{
-      name: 'vue-ts-mobile',
-      value: 'https://github.com/kspf/vue2-ts-mobile-template.git'
-    }]
+/** 
+ * git初始化项目
+ */
+function gitInit() {
+  // 初始化git仓库
+  if (shell.exec(`git init`).code !== 0) {
+    throw Error('git init fail')
   }
-]).then(answer => {
-  try{
-    // 拉取预设项目
-    if(shell.exec(`git clone ${answer['template']} ${answer['name']}`).code !== 0) {
-      throw Error('git clone fail')
-    }
-    // 切换到
-    shell.cd(`./${answer['name']}`)
-
-    // 删除.git文件夹
-    deleteFolderRecursive(path.join(destDir, `${answer['name']}/.git`))
-
-    //通过模板文件渲染
-    ejs.renderFile(path.join(destDir, `${answer['name']}/package.json`), answer, (err, result) => {
-      if (err) throw err;
-      //写入目标目录
-      fs.writeFileSync(path.join(destDir, `${answer['name']}/package.json`), result)
-
-      // 项目初始化
-      gitInit()
-    })
-  }catch(err){
-    console.error(err)
+  // 添加文件到git仓库
+  if (shell.exec(`git add .`).code !== 0) {
+    throw Error('git add fail')
   }
-})
+  // 初始化项目
+  if (shell.exec(`git commit -m "init project"`).code !== 0) {
+    throw Error('git init project fail')
+  }
+
+  shell.echo('项目初始化完毕');
+}
 
 /**
- *
- * @param {*} url
+ * 获取模板列表
  */
-function deleteFolderRecursive(url) {
+function getTemplateList (){
+  let reslove
+  let reject
+
+  https.get('https://kspf.github.io/render-custom-template/list.json', response => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+      data += chunk
+    })
+
+    response.on('end', () => {
+      reslove(JSON.parse(data))
+    })
+
+  }).on("error", (err) => {
+    reject(err)
+  })
+
+
+  return new Promise((r,j) => {
+    reslove = r
+    reject = j
+  })
+}
+
+/**
+   * 删除文件夹
+   * @param {*} url
+   */
+ function deleteFolderRecursive(url) {
   let files = [];
   /**
    * 判断给定的路径是否存在
@@ -97,22 +101,69 @@ function deleteFolderRecursive(url) {
   }
 }
 
-/** 
- * git初始化项目
-*/
-function gitInit(){
-  // 初始化git仓库
-  if(shell.exec(`git init`).code !== 0){
-    throw Error('git init fail')
-  }
-  // 添加文件到git仓库
-  if(shell.exec(`git add .`).code !== 0){
-    throw Error('git add fail')
-  }
-  // 初始化项目
-  if(shell.exec(`git commit -m "init project"`).code !== 0){
-    throw Error('git init project fail')
-  }
+/**  
+ * 开始与命令行交互
+ */
+function cli(templateList) {
 
-  shell.echo('项目初始化完毕');
+  inquirer.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Project name ?',
+    },
+    {
+      type: 'input',
+      name: 'version',
+      message: 'What version?',
+      default: '0.0.1'
+    },
+    {
+      type: 'list',
+      message: 'Please select a project template',
+      name: 'template',
+      choices: templateList
+    }
+  ]).then(answer => {
+    try {
+      // 拉取预设项目
+      if (shell.exec(`git clone ${answer['template']} ${answer['name']}`).code !== 0) {
+        throw Error('git clone fail')
+      }
+      // 切换到
+      shell.cd(`./${answer['name']}`)
+
+      // 删除.git文件夹
+      deleteFolderRecursive(path.join(destDir, `${answer['name']}/.git`))
+
+      //通过模板文件渲染
+      ejs.renderFile(path.join(destDir, `${answer['name']}/package.json`), answer, (err, result) => {
+        if (err) throw err;
+        //写入目标目录
+        fs.writeFileSync(path.join(destDir, `${answer['name']}/package.json`), result)
+
+        // 项目初始化
+        gitInit()
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  })
 }
+
+/** 
+ * 入口方法
+ * */ 
+async function main () {
+   
+  try {
+    const data = await getTemplateList()
+
+    cli(data)
+  }catch (err){
+    throw err
+  }
+  
+}
+
+// 调用
+main();
